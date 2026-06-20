abcfca9d5764969d9eb4a4292f0057f9d01c4fab
Nihashini <nihashinisuba@gmail.com>
2026-06-20 16:17:39 +0530

    M

--- frontend/src/Pages/EmployeeList.jsx
+++ frontend/src/Pages/EmployeeList.jsx
@@ -5,7 +5,8 @@ import DeleteConfirmModal from '../Components/DeleteConfirmModal';
 import { toast } from 'react-toastify';
 
 // const API_URL = 'http://localhost:5000/api/employees';
-const API_URL = 'https://employee-management-q87u.vercel.app/api';
+// const API_URL = 'https://employee-management-q87u.vercel.app/api';
+const API_URL = 'https://employee-management-q87u.vercel.app/api/employees';
 
 export default function EmployeeList() {
   const [employees, setEmployees] = useState([]);
